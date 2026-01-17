9a0af7f0bdc7be191ece76227268b3f9c76c60d3
Matthew Turney <matthew.a.turney@gmail.com>
2026-01-17 14:25:19 -0600

    Add nom for working with nix directly

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -58,5 +58,11 @@
     libreoffice
     discord
     protonvpn-gui
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
   ];
 }
