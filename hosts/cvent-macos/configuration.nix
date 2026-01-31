af7196987afeb9910a210290d4dd024a94edd340
Matthew Turney <matthew.a.turney@gmail.com>
2026-01-31 14:51:29 -0600

    Some sensible defaults for MacOS settings

--- hosts/cvent-macos/configuration.nix
+++ hosts/cvent-macos/configuration.nix
@@ -35,17 +35,56 @@
   system.defaults = {
     dock = {
       autohide = true;
+      autohide-delay = 0.5;
       orientation = "bottom";
       show-recents = false;
       tilesize = 48;
+      largesize = 52;
+      magnification = true;
+      show-process-indicators = true;
     };
     finder = {
+      AppleShowAllFiles = true;
       AppleShowAllExtensions = true;
+      ShowPathbar = true;
+      ShowStatusBar = true;
       FXEnableExtensionChangeWarning = false;
+      # Set Finder view to list by default.
       FXPreferredViewStyle = "Nlsv";
+      FXRemoveOldTrashItems = true;
+      # Search the current folder by default.
+      FXDefaultSearchScope = "SCcf";
+      NewWindowTarget = "Home";
+      ShowExternalHardDrivesOnDesktop = false;
+      ShowMountedServersOnDesktop = false;
+      ShowRemovableMediaOnDesktop = false;
+      CreateDesktop = false;
     };
+    menuExtraClock = {
+      FlashDateSeparators = false;
+      IsAnalog = false;
+      Show24Hour = true;
+      ShowAMPM = false;
+      ShowDayOfMonth = false;
+      ShowDayOfWeek = true;
+      ShowDate = 1;
+      ShowSeconds = false;
+    };
+
+    controlcenter = {
+      AirDrop = true;
+      BatteryShowPercentage = true;
+      Bluetooth = true;
+      Display = true;
+      FocusModes = true;
+      NowPlaying = true;
+      Sound = true;
+    };
+
     NSGlobalDomain = {
+      AppleShowAllFiles = true;
       AppleShowAllExtensions = true;
+      AppleShowScrollBars = "Automatic";
       InitialKeyRepeat = 15;
       KeyRepeat = 2;
     };
