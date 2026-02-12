7a6cdd270515cd2714401f3c07a75ce461e5e30d
Matthew Turney <matthew.a.turney@gmail.com>
2026-02-11 22:41:10 -0600

    A calibre plugin for ACSM files requires overriding some environment variables
    This causes a cache miss and, thus, requires compilation of Calibre.

--- modules/home/book-management.nix
+++ modules/home/book-management.nix
@@ -2,11 +2,16 @@
 
 {
   # Might put this in a server on the NAS.
-  # home.packages = with pkgs; [
-  #   libation
-  # ];
+  home.packages = with pkgs; [
+    libation
 
-  programs.calibre = {
-    enable = true;
-  };
+    # See: https://github.com/Leseratte10/acsm-calibre-plugin/issues/68#issuecomment-2162686156
+    (pkgs.calibre.overrideAttrs (old: {
+      postInstall = ''
+        wrapProgram $out/bin/calibre \
+            --set-default ACSM_LIBCRYPTO ${pkgs.openssl.out}/lib/libcrypto.so \
+            --set-default ACSM_LIBSSL ${pkgs.openssl.out}/lib/libssl.so
+      '';
+    }))
+  ];
 }
