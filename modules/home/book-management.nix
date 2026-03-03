d5298e25c1c86a17cf7fddfeffcf3a5394e7597f
Matthew Turney <pho3nixf1re@proton.me>
2026-03-03 12:03:55 -0600

    Fix calibre build by setting QMAKE and session variables for ACSM plugin
    See https://github.com/NixOS/nixpkgs/issues/493843

--- modules/home/book-management.nix
+++ modules/home/book-management.nix
@@ -4,14 +4,18 @@
   # Might put this in a server on the NAS.
   home.packages = with pkgs; [
     libation
-
-    # See: https://github.com/Leseratte10/acsm-calibre-plugin/issues/68#issuecomment-2162686156
-    (pkgs.calibre.overrideAttrs (old: {
-      postInstall = ''
-        wrapProgram $out/bin/calibre \
-            --set-default ACSM_LIBCRYPTO ${pkgs.openssl.out}/lib/libcrypto.so \
-            --set-default ACSM_LIBSSL ${pkgs.openssl.out}/lib/libssl.so
-      '';
+    # See: https://github.com/NixOS/nixpkgs/issues/493843
+    (calibre.overrideAttrs (oldAttrs: {
+      installPhase = ''
+        export QMAKE="${qt6.qtbase}/bin/qmake"
+      ''
+      + oldAttrs.installPhase;
     }))
   ];
+
+  # See: https://github.com/Leseratte10/acsm-calibre-plugin/issues/68#issuecomment-2162686156
+  home.sessionVariables = {
+    ACSM_LIBCRYPTO = "${pkgs.openssl.out}/lib/libcrypto.so";
+    ACSM_LIBSSL = "${pkgs.openssl.out}/lib/libssl.so";
+  };
 }
