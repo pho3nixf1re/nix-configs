1fa3e08f7befed86b6d62ddcf52a6b1e123861d2
Matthew Turney <pho3nixf1re@proton.me>
2026-03-25 21:37:11 -0500

    Basic wezterm configuration with theme that follows system dark/light

--- modules/home/wezterm/wezterm.nix
+++ modules/home/wezterm/wezterm.nix
@@ -4,5 +4,13 @@
   programs.wezterm = {
     enable = true;
     enableZshIntegration = true;
+    # Links to ./wezterm.lua
+    extraConfig = ''return require("config/wezterm")'';
+  };
+
+  # $XDG_CONFIG_HOME/wezterm/wezterm.lua
+  xdg.configFile."wezterm/config" = {
+    source = ./config;
+    recursive = true;
   };
 }
