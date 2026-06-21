a6de8c5de6a011edb8e5573f6484c770f555c938
Matthew Turney <pho3nixf1re@proton.me>
2026-06-20 20:55:43 -0500

    Element Desktop causes Alfred to hang on macOS, so only install on Linux.

--- modules/home/personal-apps.nix
+++ modules/home/personal-apps.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }:
 
 {
-  programs.element-desktop.enable = true;
-
   home.packages = with pkgs; [
     discord
-    element-desktop
     proton-vpn
   ] ++ lib.optionals (pkgs.stdenv.isLinux) [
     libreoffice
+    # This causes Alfred to hang on macOS, so only install on Linux due to a
+    # bug in macOS 26 (Tahoe) that causes mds to index the entire app bundle. Or
+    # something like that. Either way this specific app causes Alfred to choke.
+    element-desktop
   ];
 }
